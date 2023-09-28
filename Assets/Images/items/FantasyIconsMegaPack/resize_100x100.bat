ec875fc496a2bdb3536556ed0dd4487c94e6f3bc
watanabe@marida <watanabe.everystudio@gmail.com>
2023-09-28 15:26:25 +0900

    エリアごとのドロップアイテム画像を追加

---
+++ Assets/Images/items/FantasyIconsMegaPack/resize_100x100.bat
@@ -0,0 +1,16 @@
+REM # 環境によっては、ImageMagickのコマンドは「magick <cmd>」じゃないと動かない
+setlocal enabledelayedexpansion
+
+for /d %%f in (Level*) do (
+  echo %%f
+  cd %%f
+
+  for %%g in (*.png) do (
+      magick convert -resize x100 %%g %%g
+  )
+
+
+  cd ../
+)
+
+
